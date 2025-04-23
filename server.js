f762bfeb4fc965963bc1d3634c56fe6a27e37a86
Muhammad Aly <muhammadalizyx143@gmail.com>
2025-04-23 14:37:39 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the public directory
+
 app.use(express.static('public'));
 
-// Handle root route
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Handle login route
+
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-// Handle profile route
+
 app.get('/profile', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'profile.html'));
 });
